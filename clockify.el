6dbbb87d0fa5edc22c00c1633b304de117a4962f
Marco Dalla Stella <marco@dallastella.name>
2019-09-29 21:31:14 +0200

    Retrive projects from active workspace

--- clockify.el
+++ clockify.el
@@ -15,6 +15,9 @@
 (require 'request)
 (require 'cl)
 
+(defvar clockify--curent-user-id nil)
+(defvar clockify--active-workspace-id nil)
+
 ;;; User-Configurable Variables
 (defgroup clockify nil
   "Manage Clockify tasks from Emacs"
@@ -26,14 +29,44 @@
   :type 'string)
 
 ;;; Constants
-(defconst clockify-api-url "https://api.clockify.me/api/v1")
+(defconst clockify-api-url
+  "https://api.clockify.me/api/v1"
+  "Default Clockify API entry point.")
+
+(defconst clockify-default-headers
+  '(("Content-Type" . "application/json"))
+  "Default request headers.")
+
 
 ;;; Support Functions
+(defun clockify--generate-headers ()
+  "Generate request headers."
+  (if clockify-auth-token
+      (cons `("X-Api-Key" . ,clockify-auth-token)
+	    clockify-default-headers)
+    clockify-default-headers))
+
 (defun clockify--user-endpoint ()
-  "Clockify user endpoint"
+  "Clockify user endpoint."
   (concat clockify-api-url "/user"))
 
+(defun clockify--workspaces-endpoint ()
+  "Clockify workspaces endpoint."
+  (concat clockify-api-url "/workspaces"))
+
+(defun clockify--projects-endpoint (workspace-id)
+  "Clockify projects endpoint.
+WORKSPACE-ID the workspace id."
+  (concat clockify-api-url (concat "/workspaces/"
+				   workspace-id
+				   "/projects")))
+
 ;;; Functions
+
+(defun* clockify--error-fn (&key error-thrown &allow-other-keys)
+  "Callback to run in case of error request response.
+ERROR-THROWN is the request response data."
+  (message "Got error: %S" error-thrown))
 
 (defun clockify--query (method endpoint &optional data)
   "Send queries to Clockify APIs.
@@ -43,20 +76,28 @@ DATA is the optional body of the request"
   (let ((response (request-response-data
 		   (request endpoint
 			    :type method
-			    :headers (cons `("X-Api-Key" . ,clockify-auth-token)
-					   '(("Content-Type" . "application/json")))
-
+			    :headers (clockify--generate-headers)
 			    :sync t
 			    :parser 'json-read
-			    :error (function*
-				    (lambda (&key error-thrown &allow-other-keys&rest _)
-				      (message "Got error: %S" error-thrown)))))))
+			    :error 'clockify--error-fn))))
     response))
 
-(defun clockify-user-info ()
+(defun clockify--user-info ()
   "Retrieve user information."
-  (clockify--query "GET"
-		   (clockify--user-endpoint)))
+  (let ((response (clockify--query
+		   "GET"
+		   (clockify--user-endpoint))))
+    (setq clockify--curent-user-id (cdr (assoc 'id response)))
+    (setq clockify--active-workspace-id (cdr (assoc 'activeWorkspace response)))))
+
+(defun clockify--projects ()
+  "Retrieve current active workspace."
+  (let ((response (clockify--query
+		   "GET"
+		   (clockify--projects-endpoint clockify--active-workspace-id))))
+    response))
+
+
 
 (provide 'clockify)
 ;;; clockify.el ends here
